23f68b78606674923d77b8b08eaf4a1a953d0b16
Jasuddin <jasuddin@gmail.com>
2024-12-09 00:49:24 +0600

    Update blog-page.jsx

--- client/src/page/blog-page.jsx
+++ client/src/page/blog-page.jsx
@@ -6,6 +6,12 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1> Blog Page</h1>
+
+            <p>This is a placeholder text</p>
+            <div className="p-3 bg-light">
+                <h5 className="fs-4 fw-bold ms-4">Blog section...</h5>
+            </div>
+
             <Footer/>
         </Layout>
     );
