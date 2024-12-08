322d42c3cf2e08488003568c4d4268cda43ebb79
Jasuddin <jasuddin@gmail.com>
2024-12-09 00:48:41 +0600

    Update about-page.jsx

--- client/src/page/about-page.jsx
+++ client/src/page/about-page.jsx
@@ -7,7 +7,13 @@ const AboutPage = () => {
         <div>
             <Layout>
                 <h1>About Page</h1>
-                <Footer />
+                <p>This is a placeholder text</p>
+                <div className="p-3 bg-light">
+                    <h5 className="fs-4 fw-bold ms-4">Details about your us...</h5>
+                    <br/>
+                    <h5 className="fs-4 fw-bold ms-4">Our Team members...</h5>
+                </div>
+                <Footer/>
             </Layout>
         </div>
     );
